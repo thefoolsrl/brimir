82b3bbedeca0ccba46cbcbe9aa00ed081b237e59
Guido Scicolone <g.scicolone@thefool.it>
2020-05-20 09:57:00 +0200

    session_store switched to redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,4 @@
+config = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join("config", "redis.yml"))[Rails.env])
+db = ENV.fetch('BRIMIR_REDIS_DB', (config[:db] || 0))
+$redis = Redis.current = Redis.new(host: config[:host], port: config[:port], db: db)
+ENV['REDIS_URL'] = "#{$redis.client.id}/cache"
