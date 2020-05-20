c0e62d2d928789a9afb230ae86a812c1cfbe4a42
Guido Scicolone <g.scicolone@thefool.it>
2020-05-20 10:17:28 +0200

    fix typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.Application.config.session_store :redis_store, servers: ["#{ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')}/session"], secure: Rails.env != 'development'
+Rails.application.config.session_store :redis_store, servers: ["#{ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')}/session"], secure: Rails.env != 'development'
